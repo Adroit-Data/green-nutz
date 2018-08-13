90149ef74d8d96583ec1de945cfbcc285bdc9d82
mhussey <marc.hussey@adroitinsight.com>
2018-08-13 16:44:52 +0100

    Data Upload now saves the file to temp folder "Upload"

--- Data Inspector/Data Inspector/Controllers/LoadController.cs
+++ Data Inspector/Data Inspector/Controllers/LoadController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+using Data_Inspector.Models;
 
 namespace Data_Inspector.Controllers
 {
@@ -14,11 +16,52 @@ namespace Data_Inspector.Controllers
             return View();
         }
 
-        public ActionResult Loaded()
+        //
+        // POST: Load
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
         {
-            ViewBag.Message = "Your application description page.";
+            if (ModelState.IsValid)
+            {
+                if (file == null)
+                {
+                    ModelState.AddModelError("File", "Please Upload Your file");
+                }
+                else if (file.ContentLength > 0)
+                {
+                    int MaxContentLength = 1024 * 1024 * 3; //3 MB
+                    string[] AllowedFileExtensions = new string[] { ".txt", ".csv" };
+
+                    if (!AllowedFileExtensions.Contains(file.FileName.Substring(file.FileName.LastIndexOf('.'))))
+                    {
+                        ModelState.AddModelError("File", "Please file of type: " + string.Join(", ", AllowedFileExtensions));
+                    }
+
+                    else if (file.ContentLength > MaxContentLength)
+                    {
+                        ModelState.AddModelError("File", "Your file is too large, maximum allowed size is: " + MaxContentLength + " MB");
+                    }
+                    else
+                    {
+                        //TO:DO
+                        
+                        var fileName = Path.GetFileName(file.FileName);
+                        var path = Path.Combine(Server.MapPath("~/Content/Upload"), fileName);
+                        file.SaveAs(path);
+                        ModelState.Clear();
+                        ViewBag.Message = "File uploaded successfully";
+                    }
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Loaded(string id)
+        {
+            ViewBag.DataFile = "Data Data Data";
 
             return View();
         }
+
     }
 }
