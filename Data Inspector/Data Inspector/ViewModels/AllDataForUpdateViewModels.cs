07787bb6539550af4f972b8905bd5cc7bf9661bb
Rafal <41632132+rafal-adroit@users.noreply.github.com>
2018-10-01 10:42:07 +0100

    ViewModels

--- Data Inspector/Data Inspector/ViewModels/AllDataForUpdateViewModels.cs
+++ Data Inspector/Data Inspector/ViewModels/AllDataForUpdateViewModels.cs
@@ -6,11 +6,11 @@ using Data_Inspector.Models;
 
 namespace Data_Inspector.ViewModels
 {
-    public class AllDataForUpdateViewModels
+    public class AllDataForUpdateViewModels 
     {
         public string tableName { get; set; }
         public string rowId { get; set; }
         public List<DataForUpdate> DataForUpdate { get; set; }
-
+        // don't know why this wasn't uploaded to git hub at the first time
     }
 }
