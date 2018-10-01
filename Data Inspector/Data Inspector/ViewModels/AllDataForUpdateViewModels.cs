816a190220fdbd8c4f380324b6884a2d8b856291
Rafal <41632132+rafal-adroit@users.noreply.github.com>
2018-10-01 09:17:50 +0100

    Edit and update. Update doesn't fully work yet.

---
+++ Data Inspector/Data Inspector/ViewModels/AllDataForUpdateViewModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Data_Inspector.Models;
+
+namespace Data_Inspector.ViewModels
+{
+    public class AllDataForUpdateViewModels
+    {
+        public string tableName { get; set; }
+        public string rowId { get; set; }
+        public List<DataForUpdate> DataForUpdate { get; set; }
+    }
+}
