e1f5c721a257bf3e620853c0d2f7444bc2bd1438
mhussey <marc.hussey@adroitinsight.com>
2018-10-10 15:19:59 +0100

    Sortable started on MyLoads .... save / edit bug fixed

--- Data Inspector/Data Inspector/Scripts/tableloaddata.js
+++ Data Inspector/Data Inspector/Scripts/tableloaddata.js
@@ -35,48 +35,65 @@ TableLoadApp.controller('TableLoadController', function ($scope, tableid, $http 
         console.log("Saving data");
         
         // set the rowID that's being updated
-        $scope.rowid = r.DIRowID;
+        $scope.rowid = r.DIRowID;        
 
         // for each field/column, save to DB
-        var x;
-        for (x in r) {
+            var i = 0;
+            var length = r.length - 1;
 
+            var x;
+            for (x in r) {
 
-            $scope.value = selected[x];
+                i++;
 
-            // don't try to update the row id or anything else in r that is undefined object type
-            if (typeof $scope.value != 'undefined' && x != 'DIRowID' )
-            {
-                (function (r) {
-                    $http({
-                        method: 'POST',
-                        url: '/MyLoads/update',
-                        params: { "TableName": $scope.tableid, "RowId": $scope.rowid, "ColumnName": x, "ColumnNewValue": $scope.value }
-                    })
-                .then(function (response) {
-                    // success
-                    console.log("Saved data");
-                },
-                function (response) { // optional
-                    // failed
-                });
-                })();
+                $scope.value = selected[x];
+
+                // don't try to update the row id or anything else in r that is undefined object type
+                if (typeof $scope.value != 'undefined' && x != 'DIRowID') {
+                    (function (r) {
+                        $http({
+                            method: 'POST',
+                            url: '/MyLoads/update',
+                            params: { "TableName": $scope.tableid, "RowId": $scope.rowid, "ColumnName": x, "ColumnNewValue": $scope.value }
+                        })
+                    .then(function (response) {
+                        // success
+                        console.log("Saved data");
+                    },
+                    function (response) { // optional
+                        // failed
+                    });
+                    })();
+                }
+
+                if (i === length) {
+                    //Update View
+                    getTableLoadData($scope.tableid);                    
+                };
+
             }
-        }
 
-        //refresh the data
-        getTableLoadData($scope.tableid);
-        //Update View
-        $scope.tableload.data[idx] = angular.copy($scope.tableload.selected);
-        $scope.reset();
-
+            $scope.tableload.data[idx] = angular.copy($scope.tableload.selected);
+            $scope.reset();
+         
     };
-
-
 
     $scope.reset = function () {
         $scope.tableload.selected = {};
     };
+
+    //SORT DATA
+    $scope.headers = ["TestField1", "TestField2", "TestField3", "TestField4", "TestField5"];
+
+    $scope.sortColumn = 'TestField1';
+
+    $scope.reverseSort = false;
+
+    $scope.sortData = function (columnIndex) {
+        $scope.reverseSort = ($scope.sortColumn == $scope.headers[columnIndex]) ? !$scope.reverseSort : false;
+
+        $scope.sortColumn = $scope.headers[columnIndex];
+    }
 
 });
 
